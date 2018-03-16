d1d105a643d8e7bc6b0768f25b260a36f53677db
Sami Kangas <zumartic@gmail.com>
2018-03-16 13:24:32 +0200

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,10 @@ module.exports = (env) => {
     const isProduction = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    console.log('env', isProduction);
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -43,7 +42,8 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     };
 };
